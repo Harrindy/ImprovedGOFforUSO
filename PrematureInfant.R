11e3d957c64cc88c33022fd249c99a8b2dc1c1cf
Dewei Wang <deweiwang@stat.sc.edu>
2019-04-01 13:57:13 -0400

    Update PrematureInfant.R

--- PrematureInfant.R
+++ PrematureInfant.R
@@ -18,21 +18,21 @@ Y=Y[is.na(Y)==FALSE]
 LSM(X,Y,graph=TRUE)
 
 #############################################
-# Conduct the fixed-critical-value approach.
-TestUSO_fixed(X,Y,alpha=0.05)
-# [1] "1: reject USO; 0: do not rejct USO"
-#         Test_statistic Fixed_cv Reject_USO
-# p=1          0.1696006    0.580          0
-# p=2          0.2625633    0.676          0
-# p=infty      0.9488259    1.353          0
-
-#############################################
-# Conduct the sample-based-critical-value approach.
+# Reproduce Figure 3(b) of the manuscript
+# Conduct the test via four methods.
 set.seed(100)
-TestUSO_samplebased(X,Y,alpha=0.05)
-
+GoF4USO(X,Y,alpha=0.05,graph=TRUE)
 # [1] "1: reject USO; 0: do not rejct USO"
-#         Test_statistic Sample_cv Reject_USO
-# p=1          0.1696006 0.3301349          0
-# p=2          0.2625633 0.4761526          0
-# p=infty      0.9488259 1.1835879          0
+# [1] "Fixed_cv: critical values based on the limiting distribution at the least favorable configuration"
+# [1] "Reject_USO_fix: reject or not based on the limiting distribution at the least favorable configuration"
+# [1] "lf_cv: critical values based on the finite distribution at the least favorable configuration"
+# [1] "Reject_USO_lf: reject or not based on the finite distribution at the least favorable configuration"
+# [1] "iso_cv: critical values based on the isotonic fitting"
+# [1] "Reject_USO_iso: reject or not based on the isotonic fitting"
+# [1] "bs_cv: critical values based on the resample method"
+# [1] "Reject_USO_bs: reject or not based on the resample method"
+# 
+#            Test_statistic Fixed_cv Reject_USO_fix     lf_cv Reject_USO_lf    iso_cv Reject_USO_iso     bs_cv Reject_USO_bs
+# p=1             0.1696006    0.580              0 0.6022493             0 0.2505163              0 0.3822582             0
+# p=2             0.2625633    0.676              0 0.6869073             0 0.3801602              0 0.5171492             0
+# p=infinity      0.9488259    1.353              0 1.3300630             0 1.0709166              0 1.1902433             0
