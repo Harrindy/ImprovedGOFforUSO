9bc9930cb61906db1c43a9108e579e16814abe15
Dewei Wang <deweiwang@stat.sc.edu>
2019-04-15 10:42:45 -0400

    Update Simulation.R

--- Simulation.R
+++ Simulation.R
@@ -20,11 +20,10 @@ source("https://raw.githubusercontent.com/harrindy/ImprovedGOFforUSO/master/Curv
 #           delta_a, delta_b, delta_c, delta_d.
 # After sourcing the Curves.R file using the above command,
 # two main functions will be defined:
-#   1. R(u, mod) calcualtes the function value of a considered ODC at u;
+#   1. R(u, mod) calcualtes the function value of a ODC (defined by mod) at u;
 #   2. Data.Generate(m,n,mod) generates 
-#                               m observations from F=picked ODC and 
-#                               n observations from Uniform(0,1),
-#                           so that the resulting ODC is the picked ODC.
+#                               m observations from F=ODC (defined by mod) and 
+#                               n observations from Uniform(0,1).
 #   Options for mod include
 #     "R0", "R1", ..., "R8" (corresponding to the 9 fixed ODCs in Section 4.1)
 #     "delta_a_0",..., "delta_a_9" (the sequence of ODCs indexed by delta_a in Section 4.2)
@@ -37,7 +36,7 @@ source("https://raw.githubusercontent.com/harrindy/ImprovedGOFforUSO/master/Curv
 u=seq(0,1,length=1000)
 plot(u,R(u,mod="R4"),type="l")
 
-# To generate m=100, n=100 independent samples for R5
+# To generate m=100, n=100 independent samples for R4
 data=Data.Generate(100,100,mod="R4")
 X=data$X
 Y=data$Y
@@ -50,8 +49,8 @@ LSM(X,Y,graph=TRUE)
 GoF4USO(X,Y,alpha=0.05,graph=TRUE)
 
 
-# You can repeat these for any ODC that was considered in Section 4. All you need to change is the value of mod.
-# For example, if you want the member delta_c=8 in the third ODC sequence in Section 4.2, simply set mod="delta_c_8":
+# You can repeat this process for any ODC that was considered in Section 4. All you need to change is the value of mod.
+# For example, if you want the member delta_c=8, simply set mod="delta_c_8":
 u=seq(0,1,length=1000)
 plot(u,R(u,mod="delta_c_8"),type="l")
 data=Data.Generate(100,100,mod="delta_c_8")
